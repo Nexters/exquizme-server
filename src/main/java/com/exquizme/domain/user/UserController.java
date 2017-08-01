f67562f10de3184d5f502b3ff6ccee1ae3e749bf
GoDonghyun <godong9@gmail.com>
2017-08-02 07:03:19 +0900

    페이스북 로그인 후 리다이렉트 설정

--- src/main/java/com/exquizme/domain/user/UserController.java
+++ src/main/java/com/exquizme/domain/user/UserController.java
@@ -2,8 +2,10 @@ package com.exquizme.domain.user;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.security.Principal;
 
@@ -17,6 +19,14 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping("/")
+    public RedirectView main(Principal principal) {
+        // TODO: login 성공 후 이동할 클라이언트 페이지로 리다이렉트
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("");
+        return redirectView;
+    }
 
     @RequestMapping("/user")
     public Principal user(Principal principal) {
