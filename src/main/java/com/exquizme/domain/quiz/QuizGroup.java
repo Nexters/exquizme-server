c6a384a925db6fa11906042b0d638856c30425b9
GoDonghyun <godong9@gmail.com>
2017-08-04 21:22:06 +0900

    Add QuizGroup

---
+++ src/main/java/com/exquizme/domain/quiz/QuizGroup.java
@@ -0,0 +1,50 @@
+package com.exquizme.domain.quiz;
+
+import com.exquizme.domain.user.User;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by godong9 on 2017. 8. 4..
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString
+@Entity
+@Table(name = "quiz_groups")
+@EntityListeners({AuditingEntityListener.class})
+public class QuizGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "url")
+    private String url;
+
+    @Column(name = "title", nullable = true)
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", updatable = false)
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private Date updatedAt;
+}
