14af6489e4d17122ee0070c765e094bb0e10e54e
garin0 <garin0112@naver.com>
2017-08-05 14:47:44 +0900

    update quiz

---
+++ src/main/java/com/exquizme/domain/quiz/QuizData.java
@@ -0,0 +1,17 @@
+package com.exquizme.domain.quiz;
+
+import com.exquizme.domain.quiz.option.QuizOptionData;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * Created by godong9 on 2017. 8. 5..
+ */
+
+@Data
+public class QuizData {
+    private Long id;
+    private List<QuizOptionData> quizOptionList;
+
+}
---
+++ src/main/java/com/exquizme/domain/quiz/answer/QuizAnswerData.java
@@ -0,0 +1,12 @@
+package com.exquizme.domain.quiz.answer;
+
+import lombok.Data;
+
+/**
+ * Created by godong9 on 2017. 8. 5..
+ */
+
+@Data
+public class QuizAnswerData {
+
+}
