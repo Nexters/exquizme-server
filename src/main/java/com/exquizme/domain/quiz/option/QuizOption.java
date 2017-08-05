2fc28b7233ff45cd29c6218bddf80a524965bc92
GoDonghyun <godong9@gmail.com>
2017-08-05 10:08:50 +0900

    Add entity classes

--- src/main/java/com/exquizme/domain/quiz/option/QuizOption.java
+++ src/main/java/com/exquizme/domain/quiz/option/QuizOption.java
@@ -1,7 +1,48 @@
 package com.exquizme.domain.quiz.option;
 
+import com.exquizme.domain.quiz.Quiz;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
 /**
  * Created by godong9 on 2017. 8. 5..
  */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString
+@Entity
+@Table(name = "quiz_options")
 public class QuizOption {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "quiz_id")
+    private Quiz quiz;
+
+    @Column(name = "order")
+    private Integer order;
+
+    @Column(name = "text")
+    private String text;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", updatable = false)
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private Date updatedAt;
 }
--- src/main/java/com/exquizme/domain/quiz/result/QuizResult.java
+++ src/main/java/com/exquizme/domain/quiz/result/QuizResult.java
@@ -1,7 +1,57 @@
 package com.exquizme.domain.quiz.result;
 
+import com.exquizme.domain.quiz.group.QuizGroup;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
 /**
  * Created by godong9 on 2017. 8. 5..
  */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString
+@Entity
+@Table(name = "quiz_results")
 public class QuizResult {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "quiz_group_id")
+    private QuizGroup quizGroup;
+
+    @Column(name = "correct")
+    private Integer correct;
+
+    @Column(name = "wrong")
+    private Integer wrong;
+
+    @Column(name = "time")
+    private Integer time;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "score")
+    private Integer score;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", updatable = false)
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private Date updatedAt;
 }
