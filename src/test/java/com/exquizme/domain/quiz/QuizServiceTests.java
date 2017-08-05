aaa38e382ae6128f09e66e5dbf06ee42686ab3d3
garin0 <garin0112@naver.com>
2017-08-05 11:30:56 +0900

    Quiz Tests
    f
    f
    sd
    fds
    f
    quiz
    exit

--- src/test/java/com/exquizme/domain/quiz/QuizServiceTests.java
+++ src/test/java/com/exquizme/domain/quiz/QuizServiceTests.java
@@ -17,6 +17,9 @@ public class QuizServiceTests {
     @Autowired
     private QuizService quizService;
 
+    /**
+     * Make user test.
+     */
     @Test
     public void makeUserTest() {
         Quiz quiz = new Quiz();
