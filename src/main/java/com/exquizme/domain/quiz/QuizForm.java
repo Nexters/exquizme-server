b0215a3ae34f857f0e633c430d08053fd030cdd0
garin0 <garin0112@naver.com>
2017-08-05 13:37:15 +0900

    Add QuizForm

---
+++ src/main/java/com/exquizme/domain/quiz/QuizForm.java
@@ -0,0 +1,11 @@
+package com.exquizme.domain.quiz;
+
+/**
+ * Created by garinkim on 2017. 8. 5..
+ */
+public class QuizForm {
+    private String text;
+    private QuizType type;
+    private String[] options;
+    private int answerIdx;
+}
