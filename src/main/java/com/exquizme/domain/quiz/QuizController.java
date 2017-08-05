779630a4459d2b265486ba88249325e4229e135d
garin0 <garin0112@naver.com>
2017-08-05 10:27:05 +0900

    update

--- src/main/java/com/exquizme/domain/quiz/QuizController.java
+++ src/main/java/com/exquizme/domain/quiz/QuizController.java
@@ -5,6 +5,7 @@ import com.exquizme.domain.user.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -27,4 +28,6 @@ public class QuizController {
     public User getQuizzes(Principal principal) {
         return userService.getCurrentUser(principal);
     }
+
+    
 }
--- src/main/java/com/exquizme/domain/quiz/QuizService.java
+++ src/main/java/com/exquizme/domain/quiz/QuizService.java
@@ -14,4 +14,5 @@ public class QuizService {
     @Autowired
     private QuizRepository quizRepository;
 
+
 }
